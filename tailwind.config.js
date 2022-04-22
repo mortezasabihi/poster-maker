9fe58a10a7bce4980e58e101fbe79d5725f039ef
Morteza Sabihi <mortezasabihi87@gmail.com>
2022-04-22 22:27:08 +0430

    style: google font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
-/* eslint-disable global-require */
+/* eslint-disable  */
 module.exports = {
   content: ['./src/**/*.{html,js,ts,jsx,tsx}', './public/index.html'],
   theme: {
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif']
+    },
     extend: {}
   },
-  plugins: [require('@tailwindcss/forms')]
+  plugins: [
+    require('@tailwindcss/forms')({
+      strategy: 'class'
+    })
+  ]
 };
