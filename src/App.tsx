d2748303440820d44bd294b960c427c03f0c666e
Morteza Sabihi <mortezasabihi87@gmail.com>
2022-04-22 17:58:18 +0430

    build: tailwind theme generator added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import type { FC } from 'react';
+import { ThemeProvider } from '~/src/context/theme-context';
 import Router from '~/src/Router';
 
 const App: FC = () => {
   return (
-    <>
+    <ThemeProvider>
       <Router />
-    </>
+    </ThemeProvider>
   );
 };
 
